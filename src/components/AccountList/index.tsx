6497cc6205f181c2835d9e00e89d851a36ddec01
Maxim-zkn <maxim-zkn@outlook.com>
2024-06-22 13:24:54 +0300

    user friendly address

--- src/components/AccountList/index.tsx
+++ src/components/AccountList/index.tsx
@@ -1,29 +1,24 @@
 import { FC} from "react";
 import { Box, Button, Input, Typography } from "@mui/material";
 import styles from './AccountList.module.css';
-import { useTonAddress } from "@tonconnect/ui-react";
+import { toUserFriendlyAddress } from "@tonconnect/ui-react";
 import { MULTISIG_LS_KEY } from "src/common/consts";
 import useTonWalletFromApi from "src/common/hooks/useTonWalletFromApi";
 
 const AccountList: FC = () => {
-
-  const userFriendlyAddress = useTonAddress();
-
   const userMultisigAdress = localStorage.getItem(MULTISIG_LS_KEY)
-
   const multisigData = useTonWalletFromApi(userMultisigAdress)
 
   return (
     <Box paddingX={'15px'}>
-      {userFriendlyAddress}
-      <Typography variant="h6" marginBottom={'24px'}>
+      <Typography variant="h6" marginBottom={'5px'}>
         My multisig accounts:
       </Typography>
       <Box className={styles.acc_list}>
         {userMultisigAdress && (
           <Box key={userMultisigAdress} className={styles.acc_item} display='flex' flexDirection={"column"}>
             <Typography variant="body2">
-              <Input type="text" value={userMultisigAdress} style={{width: 240}}/>
+              <Input type="text" value={toUserFriendlyAddress(userMultisigAdress)} className={styles.tonAdderss}/>
             </Typography>
             <Typography variant="body2">
               Balance:
